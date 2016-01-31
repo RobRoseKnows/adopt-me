f3e25db8ebba808c2ff86d7ecfb63fb30adf35e9
Ati Ok <ao10@umbc.edu>
2016-01-31 04:06:37 -0500

    Finished generateData.java

--- app/src/main/java/io/robrose/hoya/adoptme/GenerateData.java
+++ app/src/main/java/io/robrose/hoya/adoptme/GenerateData.java
@@ -1,7 +1,10 @@
-package io.robrose.hoya.adoptme.data;
+package io.robrose.hoya.adoptme;
 import android.content.ContentValues;
 import java.util.Random;
 import java.util.Random;
+
+import io.robrose.hoya.adoptme.data.DogContract;
+
 /**
  * Created by Ati on 1/31/16.
  */
@@ -20,7 +23,7 @@ public class GenerateData {
             socialValues.put(DogContract.SocialsEntry.COLUMN_SNAPCHAT, handles[index]);
             socialValues.put(DogContract.SocialsEntry.COLUMN_HASHTAG, handles[index]);
             socialValues.put(DogContract.SocialsEntry.COLUMN_TWITTER, handles[index]);
-            cvs[index] = socialValues;
+            cvs[i] = socialValues;
         }
         return cvs;
     }
@@ -36,24 +39,31 @@ public class GenerateData {
             shelterValues.put(DogContract.ShelterEntry.COLUMN_LONG, 35);
             shelterValues.put(DogContract.ShelterEntry.COLUMN_ADDRESS, "Funkytown");
             shelterValues.put(DogContract.ShelterEntry.COLUMN_CITY, "Georgetown");
-            cvs[index] = shelterValues;
+            cvs[i] = shelterValues;
         }
         return cvs;
     }
-    public static ContentValues[] generateDogs(int n, int shelters)
-    {
+    public static ContentValues[] generateDogs(int n, int shelters) {
         ContentValues[] cvs = new ContentValues[n];
-        for (int i = 0; i < n; i++){
+        String[] pictures = {"http://i.imgur.com/yR4ykiA.jpg", "http://i.imgur.com/avPgJ9t.jpg",
+                "http://i.imgur.com/By5US9k.jpg", "http://i.imgur.com/hCAJg1Z.jpg",
+                "http://i.imgur.com/kbsllgP.jpg", "http://i.imgur.com/HyMOyQt.jpg",
+                "http://i.imgur.com/mBAvAcQ.jpg", "http://i.imgur.com/AerijYH.jpg",
+                "http://i.imgur.com/dhdYDYP.jpg", "http://i.imgur.com/vpi1OMb.jpg"};
+        for (int i = 0; i < n; i++) {
+            ContentValues dogValues = new ContentValues();
             int index = new Random().nextInt(handles.length);
-            shelterValues.put(DogContract.DogEntry.COLUMN_NAME, locationRowId);
-        shelterValues.put(DogContract.DogEntry.COLUMN_AGE, TEST_DATE);
-        shelterValues.put(DogContract.DogEntry.COLUMN_ENTERED, 1.1);
-        shelterValues.put(DogContract.DogEntry.COLUMN_BIO, "Playful");
-        shelterValues.put(DogContract.DogEntry.COLUMN_SHELTER_KEY, getRandomNumber());
-        shelterValues.put(DogContract.DogEntry.COLUMN_ALBUM, //url);shelterValues.put(DogContract.DogEntry.COLUMN_INTEREST, 65);
-        shelterValues.put(DogContract.DogEntry.COLUMN_INTERNAL, "Asteroids");
+            int indexPictures = new Random().nextInt(pictures.length);
+            dogValues.put(DogContract.DogEntry.COLUMN_NAME, handles[index]);
+            dogValues.put(DogContract.DogEntry.COLUMN_AGE, ((Math.random() * 143) + 1));
+            dogValues.put(DogContract.DogEntry.COLUMN_ENTERED, 1453607252);
+            dogValues.put(DogContract.DogEntry.COLUMN_BIO, "Playful");
+            dogValues.put(DogContract.DogEntry.COLUMN_SHELTER_KEY, (Math.random() * shelters));
+            dogValues.put(DogContract.DogEntry.COLUMN_ALBUM, pictures[indexPictures]);
+            dogValues.put(DogContract.DogEntry.COLUMN_INTEREST, "TBD");
+            dogValues.put(DogContract.DogEntry.COLUMN_INTERNAL, "Asteroids");
+            cvs[i] = dogValues;
         }
-
-}
-
+        return cvs;
+    }
 }
