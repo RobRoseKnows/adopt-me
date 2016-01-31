06015819cbd60979294fe065228767e5c00e676c
Ati Ok <ao10@umbc.edu>
2016-01-31 03:38:38 -0500

    Working on generateDog

--- app/src/main/java/io/robrose/hoya/adoptme/GenerateData.java
+++ app/src/main/java/io/robrose/hoya/adoptme/GenerateData.java
@@ -26,22 +26,34 @@ public class GenerateData {
     }
     public static ContentValues[] generateShelters(int n) {
         ContentValues[] cvs = new ContentValues[n];
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
+            int index = new Random().nextInt(handles.length);
             ContentValues shelterValues = new ContentValues();
-            shelterValues.put(DogContract.ShelterEntry.COLUMN_FACEBOOK, handles[index]);
-            shelterValues.put(DogContract.SocialsEntry.COLUMN_HASHTAG, handles[index]);
-            shelterValues.put(DogContract.SocialsEntry.COLUMN_INSTAGRAM, handles[index]);
-            shelterValues.put(DogContract.SocialsEntry.COLUMN_SNAPCHAT, handles[index]);
-            shelterValues.put(DogContract.SocialsEntry.COLUMN_HASHTAG, handles[index]);
-            shelterValues.put(DogContract.SocialsEntry.COLUMN_TWITTER, handles[index]);
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_NAME, handles[index]);
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_ABOUT, "About this shelter");
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_SOCIALS_KEY, i);
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_LAT, 37);
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_LONG, 35);
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_ADDRESS, "Funkytown");
+            shelterValues.put(DogContract.ShelterEntry.COLUMN_CITY, "Georgetown");
+            cvs[index] = shelterValues;
         }
+        return cvs;
     }
+    public static ContentValues[] generateDogs(int n, int shelters)
+    {
+        ContentValues[] cvs = new ContentValues[n];
+        for (int i = 0; i < n; i++){
+            int index = new Random().nextInt(handles.length);
+            shelterValues.put(DogContract.DogEntry.COLUMN_NAME, locationRowId);
+        shelterValues.put(DogContract.DogEntry.COLUMN_AGE, TEST_DATE);
+        shelterValues.put(DogContract.DogEntry.COLUMN_ENTERED, 1.1);
+        shelterValues.put(DogContract.DogEntry.COLUMN_BIO, "Playful");
+        shelterValues.put(DogContract.DogEntry.COLUMN_SHELTER_KEY, getRandomNumber());
+        shelterValues.put(DogContract.DogEntry.COLUMN_ALBUM, //url);shelterValues.put(DogContract.DogEntry.COLUMN_INTEREST, 65);
+        shelterValues.put(DogContract.DogEntry.COLUMN_INTERNAL, "Asteroids");
+        }
+
 }
-//shelterValues.put(DogContract.DogEntry.COLUMN_NAME, locationRowId);
-//        shelterValues.put(DogContract.DogEntry.COLUMN_AGE, TEST_DATE);
-//        shelterValues.put(DogContract.DogEntry.COLUMN_ENTERED, 1.1);
-//        shelterValues.put(DogContract.DogEntry.COLUMN_BIO, "Playful");
-//        shelterValues.put(DogContract.DogEntry.COLUMN_SHELTER_KEY, );
-//        shelterValues.put(DogContract.DogEntry.COLUMN_ALBUM, //url);
-//        shelterValues.put(DogContract.DogEntry.COLUMN_INTEREST, 65);
-//        shelterValues.put(DogContract.DogEntry.COLUMN_INTERNAL, "Asteroids");
+
+}
